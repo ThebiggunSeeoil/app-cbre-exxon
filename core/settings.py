3baba09c0bbce6df646b39bac40293e2c9778fff
yutthachai thongkumchum <yutthachai_t@hotmail.com>
2020-12-13 22:48:49 +0700

    add host

--- core/settings.py
+++ core/settings.py
@@ -19,11 +19,11 @@ PROJECT_DIR = Path(__file__).parent
 SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True)
+DEBUG = config('DEBUG', default=False)
 # DEBUG = False
 
 # load production server from .env
-ALLOWED_HOSTS = ['cbre-exxon-app.herokuapp.com', '8ff8774654dc.ngrok.io','127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['git.heroku.com/cbre-exxon-apps.git', '8ff8774654dc.ngrok.io','127.0.0.1', config('SERVER', default='127.0.0.1')]
 #ALLOWED_HOSTS = ['cbre-exxon-app.herokuapp.com']
 
 # Application definition

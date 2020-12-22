a7eac12298b8f36b164904fe2918649ba08e0f52
ThebiggunSeeoil <yutthachai_t@hotmail.com>
2020-12-22 09:15:16 +0700

    revised allow host

--- core/settings.py
+++ core/settings.py
@@ -23,8 +23,8 @@ DEBUG = config('DEBUG', default=False)
 # DEBUG = False
 
 # load production server from .env
-ALLOWED_HOSTS = ['https://cbre-exxon-appss.herokuapp.com', '5e00eb5d27dc.ngrok.io','127.0.0.1', config('SERVER', default='127.0.0.1')]
-#ALLOWED_HOSTS = ['cbre-exxon-app.herokuapp.com']
+# ALLOWED_HOSTS = ['https://cbre-exxon-appss.herokuapp.com', '5e00eb5d27dc.ngrok.io','127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['cbre-exxon-appss.herokuapp.com']
 
 # Application definition
 

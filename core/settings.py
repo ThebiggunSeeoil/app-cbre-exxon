cf7aabc8d9e024bbe458fd83dec5c68e75fe9ed8
ThebiggunSeeoil <yutthachai_t@hotmail.com>
2020-12-22 09:05:54 +0700

    add heroku

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ DEBUG = config('DEBUG', default=False)
 # DEBUG = False
 
 # load production server from .env
-ALLOWED_HOSTS = ['git.heroku.com/cbre-exxon-apps.git', '5e00eb5d27dc.ngrok.io','127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['https://cbre-exxon-appss.herokuapp.com', '5e00eb5d27dc.ngrok.io','127.0.0.1', config('SERVER', default='127.0.0.1')]
 #ALLOWED_HOSTS = ['cbre-exxon-app.herokuapp.com']
 
 # Application definition

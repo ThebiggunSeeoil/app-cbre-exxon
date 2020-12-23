dbc14208831f033f9586a43e473daf1f521ae67d
ThebiggunSeeoil <yutthachai_t@hotmail.com>
2020-12-23 08:17:26 +0700

    firstcommit

---
+++ .env-cbre/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yutthachaithongkumchum/myproject/app-cbre-exxon/app-cbre-exxon/.env-cbre/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
